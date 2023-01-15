0d022c0a63ef674a6bdbfe023d335a09b458876b
dnln@feip.co <hadevswork@gmail.com>
2023-01-15 19:45:32 -0300

    fixed token

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import replicate;
 import os;
 
 bot = telebot.TeleBot('5891533811:AAFCWgHoJkssbbJvpl25c-D0Pqo-jsG8HqI');
+client = replicate.Client(api_token="cc1669eb7f7637e42fe1d3212ab87e654d8ac819")
 
 x = True
 starting = ""
@@ -34,12 +35,12 @@ def generate(message):
     
 def generate_video_with_ai(start_scene, end_scene,message):
 	try:
-		model = replicate.models.get("andreasjansson/stable-diffusion-animation")
+		model = client.models.get("andreasjansson/stable-diffusion-animation")
 		version = model.versions.get("ca1f5e306e5721e19c473e0d094e6603f0456fe759c10715fcd6c1b79242d4a5")
 		output = version.predict(prompt_start = start_scene, prompt_end = end_scene, output_format="mp4", gif_frames_per_second = "20", gif_ping_pong = True, guidance_scale = 7.5, prompt_strength = 0.9, film_interpolation = True,num_inference_steps = 50, num_animation_frames = 25, num_interpolation_steps =  5)
 		print("result is:", output)
 		return output
-	except replicate.exceptions.ModelError:
+	except client.exceptions.ModelError:
 		bot.send_message(
         message.chat.id, 
         'Кажется, что данный запрос содержит 18+ контент. Попробуйте еще раз или измените запрос.'
